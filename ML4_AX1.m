df483c624899fe3c068540c18c917e116d6d1a66
Toni <rincerwind@gmail.com>
2015-03-16 01:28:07 +0000

    More changes to layout

--- ML4_AX1.m
+++ ML4_AX1.m
@@ -30,11 +30,11 @@ function ML4_AX1()
   train_size = round(0.7 * m);
   
   rand_indx = randperm(m);
-  %train_indx = rand_indx(1:train_size);
-  %test_indx = rand_indx(train_size + 1:end);
+  train_indx = rand_indx(1:train_size);
+  test_indx = rand_indx(train_size + 1:end);
   
-  train_indx = 1:train_size;
-  test_indx = train_size + 1:m;
+  %train_indx = 1:train_size;
+  %test_indx = train_size + 1:m;
   
   red_X = red(train_indx,1:end - 1);
   red_t = red(train_indx, end);
@@ -114,7 +114,7 @@ function ML4_AX1()
   % -----------------------------------------------------------------------
   
   
-  %% Task 2 - Reguralized Linear Regression -------------------------------
+  %% Task 2 - Reguralized Linear Regression
   lambdas = [0, 0.01, 0.05, 0.1, 0.5, 1, 2, 5, 10];
   loss = ones(length(lambdas),1);
   
@@ -215,8 +215,6 @@ function ML4_AX1()
   mean_CV = mean(cv_loss,2);
   mean_Test = mean(test_loss,2);
   
-  size(mean_Train)
-  
   [min_loss, min_indx] = min(mean_Test);
   redLinearReg_loss = min_loss
   best_lambda = lambdas(min_indx)
@@ -233,9 +231,91 @@ function ML4_AX1()
   hold off;
   % -----------------------------------------------------------------------
   
-  %% Task 3 - K-Nearest Neighbours ----------------------------------------
+  %% Task 3 - K-Nearest Neighbours
   
   % Do a 10-Fold KNN
-  knnCV([red_X; red_Xtest], [red_t; red_Ttest], [1, 3, 7, 11, 19], 10);
+  X = [red_X; red_Xtest];
+  t = [red_t; red_Ttest];
+  kVals = [1, 3, 7, 11, 19];
+  N = 10;
+  m = size(X,1);
+  err = zeros(length(kVals),N);
+  
+  fold_size = floor(m/N);
+  unique_t = unique(t);
+  
+  fold_start = ones(1,N);
+  fold_end = ones(1,N);
+  fold_end(1) = fold_start(1) + fold_size - 1;
+  
+  % Pre-compute fold sizes
+  for fold=2:N
+    fold_start(fold) = fold_end(fold-1) + 1;
+    if( fold_start(fold) + fold_size - 1 > m || fold == N )
+       fold_end(fold) = m;
+    else
+       fold_end(fold) = fold_start(fold) + fold_size - 1;
+    end
+  end
+  
+  % Loop over values of K for KNN
+  for k=1:length(kVals)
+    K = kVals(k);
+    
+    % Loop over the N folds
+    for fold=1:N
+      fStart = fold_start(fold);
+      fEnd = fold_end(fold);
+      
+      testX = X(fStart:fEnd,:);
+      testT = t(fStart:fEnd,:);
+      
+      trainX = X;
+      trainX(fStart:fEnd,:) = [];
+      trainT = t;
+      trainT(fStart:fEnd,:) = [];
+    
+      train_m = size(trainX,1);
+      test_m = size(testX,1);
+       
+      % KNN for all test cases
+      c_predict = zeros(test_m,1);
+      for i=1:test_m
+          % replicate testX(i,:) in train_m row, so we can efficiently 
+          % find the distances from the current testX to all trainX points
+          rep_testX = repmat(testX(i,:), train_m, 1);
+          
+          % sum row-wise as 1 row is 1 point
+          dists = sum((trainX - rep_testX) .^ 2, 2);
+          
+          % sorts dists and returns the sorted array, 
+          % as well as previous indecies of the current elements
+          [sDists, inds] = sort(dists,'ascend');
+          [vals, bins] = hist(trainT(inds(1:K)), unique_t);
+          
+          % max can return the index of the max element, 
+          % but there can be many equal max elements
+          max_val = max(vals);
+          max_pos = find(vals == max_val);
+          
+          % there can be more than one class with a max
+          % in this case choose randomly
+          if length(max_pos)>1
+            rand_max = randperm(length(max_pos));
+            max_pos = max_pos(rand_max(1));
+          end
+          c_predict(i) = bins(max_pos);
+      end
+      
+      % perform 0/1 loss (Accuracy)
+      err(k, fold) = sum(c_predict~=testT)/length(testT);
+    end % end of fold loop
+  end % end of K loop
+  
+  figure('Name', '10-Fold KNN');
+  plot(kVals, mean(err,2), 'b-o');
+  title('10-Fold Cross Validation, KNN');
+  xlabel('K');
+  ylabel('Error');
   % -----------------------------------------------------------------------
 end
